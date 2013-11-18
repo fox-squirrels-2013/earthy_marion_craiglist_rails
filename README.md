8049370e4e1857693381cc957f7eb540109fc7b0
lindseysugino <lindseysugino@gmail.com>
2013-11-18 12:20:29 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 earthy_marion_craiglist_rails
 =============================
+
+## DBC Rails Project: Make rails Craigslist JR
+
+https://socrates.devbootcamp.com/challenges/350
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EarthyMarionCraiglistRails::Application.config.secret_token = 'd964ae8007b9035befc07603e83181f56ba54b25a42fb5df2be6c395c6307bc4ec802d0f3b8ae886869ebac7f5babfe4a02f15b94d9817716dbe8b214570c86a'
